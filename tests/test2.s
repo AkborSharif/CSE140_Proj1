889f8bba4b63a027e1a8287ac2641d572377e4f7
AkborSharif <36203064+AkborSharif@users.noreply.github.com>
2018-10-10 08:51:49 -0700

    test2.s

---
+++ tests/test2.s
@@ -0,0 +1,17 @@
+.text 
+
+      addi $t0, $0, 10
+      addi $t1, $0, 12
+      
+      sub $t2, $t1, $t0
+      
+      srl $t3, $t1, 2
+      
+      and $t4, $t0, $t1
+      
+      andi $t5, $t1, 4
+      
+      or $t6, $t0, $t1
+    
+      slt $t7, $t0, $t1
+
